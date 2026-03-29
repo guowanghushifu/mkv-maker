18537b3503dde9fac375ed14445144ef32c04b0a
guowanghushifu <80014670+guowanghushifu@users.noreply.github.com>
2026-03-29 10:28:20 +0800

    fix: align mkvmerge command builder with task 6 spec

--- internal/remux/command_builder.go
+++ internal/remux/command_builder.go
@@ -1,38 +1,28 @@
 package remux
 
-func BuildMKVMergeArgs(draft Draft) []string {
-	args := []string{"--output", draft.OutputPath}
+import "strconv"
 
-	if draft.Playlist != "" {
-		args = append(args, "--playlist", draft.Playlist)
-	}
-	if draft.EnableDV {
-		args = append(args, "--engage", "enable_dolby_vision")
-	}
-	if draft.Video.Name != "" {
-		args = append(args, "--track-name", "0:"+draft.Video.Name)
+func BuildMKVMergeArgs(d Draft) []string {
+	args := []string{"--output", d.OutputPath}
+
+	if d.Video.Name != "" {
+		args = append(args, "--track-name", "0:"+d.Video.Name)
 	}
 
-	for _, track := range draft.Audio {
-		if !track.Selected {
-			continue
+	for index, track := range d.Audio {
+		audioSelector := strconv.Itoa(index + 1)
+
+		args = append(args, "--language", audioSelector+":"+track.Language)
+		args = append(args, "--track-name", audioSelector+":"+track.Name)
+		if track.Selected && track.Default {
+			args = append(args, "--default-track-flag", audioSelector+":yes")
 		}
-		if track.Language != "" {
-			args = append(args, "--language", track.ID+":"+track.Language)
-		}
-		if track.Name != "" {
-			args = append(args, "--track-name", track.ID+":"+track.Name)
-		}
-		if track.Default {
-			args = append(args, "--default-track", track.ID+":yes")
-			continue
-		}
-		args = append(args, "--default-track", track.ID+":no")
 	}
 
-	if draft.SourcePath != "" {
-		args = append(args, draft.SourcePath)
+	if d.EnableDV {
+		args = append(args, "--engage", "merge_dolby_vision")
 	}
 
+	args = append(args, d.SourcePath)
 	return args
 }
